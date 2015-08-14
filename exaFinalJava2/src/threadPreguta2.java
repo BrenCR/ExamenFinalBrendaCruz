3eebd10f5230d5e42b422a21955af0499b99f8a0
T-107 <T-107@PC247>
2015-08-13 19:33:01 -0500

    130815

---
+++ exaFinalJava2/src/threadPreguta2.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author T-107
+ */
+public class threadPreguta2 {
+    public void run(){
+        while (true){
+        try{
+            Thread.sleep(500);
+        }catch(InterruptedException e){
+            
+            }
+            
+        }
+    }
+    
+}
+
+/*
+pregunta 1
+yo lo subo ahorita
+pregunta 2
+ public class Threadanonimo implements Runnable{
+    public static void main(String[] args) {
+        Runnable r=new Threadanomino();
+        Thread t1=new Thread(r);
+        t1.start();
+        
+    }
+pregunta 4
+public class ventanita extend java.swing.Jframe{
+    int x=300; int y=180;
+    public ventana(){
+        initcomponents();
+        setsize(x,y);
+    }
+}
+pregunta 5
+public class iniciothread{
+    public static void main(string []arg){
+        primerthread t=new primerthread();
+        t.start()
+    }
+}
+pregunta 6
+public class threadDescanso{
+    public void run(){
+        while(true){
+            try{
+                thread.sleeo(500);
+            }carch(interrupted Exception e){
+            
+        }
+            }
+        }
+    }
+}*/
